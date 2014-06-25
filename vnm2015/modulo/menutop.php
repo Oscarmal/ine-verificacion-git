57bc2f68b4d66b0ad4c09a0898f1bd5268803096
israel <israel.delangel@ine.mx>
2014-06-25 14:43:42 -0500

    se agrega la funcionalidad para el reporte de inicio de enumeracion

--- vnm2015/modulo/menutop.php
+++ vnm2015/modulo/menutop.php
@@ -1,10 +1,11 @@
-<?php session_name('ddvc'); session_start(); include_once($_SESSION['header_path']);?>
+ <?php session_name('ddvc'); session_start(); include_once($_SESSION['header_path']);?>
 <?php
 $vista = 'menutop.html';
 $tpl_data = array( 
 			 MORE => '<link href="'.$Path[css].'menutop.css" rel="stylesheet" type="text/css">'
 			,URL1 => $Path[src].'php/vnm.php?a=inicio'
 			,URL2 => $Path[src].'php/vnm.php?a=enumeracion'
+				,URL21 => $Path[src].'php/vnm.php?a=rep_inicio_enumera'
 			,URL3 => $Path[src].'php/vnm.php?a=cobertura'
 				,URL31 => $Path[src].'php/vnm.php?a=cobertura'
 				,URL32 => $Path[src].'php/vnm.php?a=cobertura'
---
+++ vnm2015/src/php/enumeracion/rep_inicio_enumera.php
@@ -0,0 +1,12 @@
+<?php session_name('ddvc'); session_start(); include_once($_SESSION['header_path']);?>
+<?php /*O3M*/
+/**
+* Descripcion:	Autentificación de usuario
+* Creación:		2014-06-19
+* @author 		Oscar Maldonado - O3M
+*
+*/
+
+//echo json_encode($Result);
+/*O3M*/
+?>
